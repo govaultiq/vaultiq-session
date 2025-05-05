a6346341e2993429a1c07534b54ba2b43cb6936f
rajumb0232 <rajugowda0232@gmail.com>
2025-05-05 11:55:55 +0530

    added few TODOs to the VaultiqSessionManager

--- src/main/java/vaultiq/session/core/VaultiqSessionManager.java
+++ src/main/java/vaultiq/session/core/VaultiqSessionManager.java
@@ -18,8 +18,10 @@ public interface VaultiqSessionManager {
 
     int totalUserSessions(String userId);
 
-    // TODO:blocklist all user sessions -> Can be used to log out from all devices
-    // TODO:blocklist all user sessions excluding string...-> Can be used to log out from all devices with exclusions
-    // TODO:blocklist session by ID -> Can be used to log out from one device.
-
+    // TODO: Get LastActive At by Session -> Can be used to check when the session was active last time.
+    // TODO: Blocklist all user sessions -> Can be used to log out from all devices.
+    // TODO: Blocklist all user sessions excluding string vararg -> Can be used to log out from all devices with exclusions.
+    // TODO: Blocklist session by ID -> Can be used to log out from one device.
+    // TODO: Check if the session blocklisted -> Can be used to check if a session is blocked.
+    // TODO: Get All blocklisted sessions by user -> Can be used to get all blocked sessions for a user.
 }
