1b839fc5c694026565640b3a7913cfd1f870b1a1
rajumb0232 <rajugowda0232@gmail.com>
2025-05-13 15:42:48 +0530

    added new field lastUpdated At, to the SessionIds wrapper to track when was the cache last updated

--- src/main/java/vaultiq/session/cache/model/SessionIds.java
+++ src/main/java/vaultiq/session/cache/model/SessionIds.java
@@ -1,10 +1,12 @@
 package vaultiq.session.cache.model;
 
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SessionIds {
     private Set<String> sessionIds = new HashSet<>();
+    private long lastUpdated; // epoch millis
 
     public Set<String> getSessionIds() {
         return sessionIds;
@@ -12,9 +14,19 @@ public class SessionIds {
 
     public void setSessionIds(Set<String> sessionIds) {
         this.sessionIds = sessionIds != null ? sessionIds : new HashSet<>();
+        updatedNow();
     }
 
     public void addSessionId(String sessionId) {
         sessionIds.add(sessionId);
+        updatedNow();
+    }
+
+    public long getLastUpdated() {
+        return lastUpdated;
+    }
+
+    private void updatedNow() {
+        this.lastUpdated = Instant.now().toEpochMilli();
     }
 }
