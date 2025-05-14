1e9d96119d6fad0b69a4479fe513de67607c5e16
rajumb0232 <rajugowda0232@gmail.com>
2025-05-14 07:49:12 +0530

    Documented VaultiqPersistenceMethod

--- src/main/java/vaultiq/session/config/annotation/model/VaultiqPersistenceMethod.java
+++ src/main/java/vaultiq/session/config/annotation/model/VaultiqPersistenceMethod.java
@@ -1,6 +1,33 @@
 package vaultiq.session.config.annotation.model;
 
+/**
+ * Enumeration defining the individual persistence methods that can be enabled
+ * for a Vaultiq session data model.
+ * <p>
+ * This enum represents the specific storage mechanisms (cache or JPA) that can be
+ * configured for a given {@link vaultiq.session.cache.model.ModelType}. It is often used
+ * in configuration properties or annotations (like {@link vaultiq.session.config.annotation.ConditionalOnVaultiqModelConfig})
+ * to specify which method of persistence is active for a particular model.
+ * </p>
+ * <p>
+ * Unlike {@link VaultiqPersistenceMode}, which describes the *combination* of methods used
+ * (e.g., CACHE_ONLY, JPA_AND_CACHE), this enum represents the atomic options.
+ * </p>
+ *
+ * @see vaultiq.session.config.annotation.ConditionalOnVaultiqModelConfig Annotation using this enum to condition beans.
+ * @see vaultiq.session.cache.model.ModelType Defines the different types of session data models.
+ * @see VaultiqPersistenceMode Defines combinations of persistence methods.
+ */
 public enum VaultiqPersistenceMethod {
+    /**
+     * Indicates that caching is enabled and should be used for persistence
+     * of a specific data model.
+     */
     USE_CACHE,
+
+    /**
+     * Indicates that JPA (database persistence) is enabled and should be used
+     * for persistence of a specific data model.
+     */
     USE_JPA
 }
